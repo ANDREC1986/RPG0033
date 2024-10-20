c0b893edb12fefccbf6a5e1a0626f1a1e077fd79
André Carvalhal <andrecar2021@hotmail.com>
2024-10-20 04:31:58 -0300

    Reapply "Trabalho concluido, Microatividades deletadas"
    This reverts commit a61ea7806a67869d90ede1f57d73750411fcee20.

--- ma4.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-pd.set_option('display.max_rows',9999)
-data = pd.read_csv('data.csv',sep=';',engine='python',encoding='UTF8')
-data2 = data.filter(['Duration','Date', 'Pulse'])
-print(data.shape)
-print(data.isnull().sum().sum())
-print(data.dtypes)
-print(data.info())
